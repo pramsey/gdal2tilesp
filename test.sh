41a3570edbb0c504e3f476e9e98f84e5885e4e2f
Rob | Labs <roblabs@users.noreply.github.com>
2016-08-15 18:32:43 -0700

    Add test script

---
+++ test.sh
@@ -0,0 +1,7 @@
+python gdal2tilesp.py -z 7-15 -w all 324511630_Descanso_FSTopo.3857.vrt
+# python gdal2tilesp.py -z 12 -w --resume all 324511630_Descanso_FSTopo.3857.vrt
+# python gdal2tilesp.py -z 13 -w --resume all 324511630_Descanso_FSTopo.3857.vrt
+# python gdal2tilesp.py -z 14 -w --resume all 324511630_Descanso_FSTopo.3857.vrt
+# python gdal2tilesp.py -z 15 -w --resume all 324511630_Descanso_FSTopo.3857.vrt
+
+mb-util 324511630_Descanso_FSTopo.3857 324511630_Descanso_FSTopo.3857.mbtiles
