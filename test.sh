8f8b03bd3bd27d3062370cf20fb48fe58fd4b884
Rob | Labs <roblabs@users.noreply.github.com>
2016-08-18 22:28:15 -0700

    chmod

--- test.sh
+++ test.sh
@@ -4,4 +4,8 @@ python gdal2tilesp.py -z 7-15 -w all 324511630_Descanso_FSTopo.3857.vrt
 # python gdal2tilesp.py -z 14 -w --resume all 324511630_Descanso_FSTopo.3857.vrt
 # python gdal2tilesp.py -z 15 -w --resume all 324511630_Descanso_FSTopo.3857.vrt
 
-mb-util 324511630_Descanso_FSTopo.3857 324511630_Descanso_FSTopo.3857.mbtiles
+# mb-util 324511630_Descanso_FSTopo.3857 324511630_Descanso_FSTopo.3857.mbtiles
+json -I -f 324511630_Descanso_FSTopo.3857/metadata.json -e 'this.name="324511630_Descanso_FSTopo"'
+json -I -f 324511630_Descanso_FSTopo.3857/metadata.json -e 'this.geotiff="324511630_Descanso_FSTopo.tif"'
+json -I -f 324511630_Descanso_FSTopo.3857/metadata.json -e 'this.geopdf="324511630_Descanso_FSTopo.pdf"'
+json -I -f 324511630_Descanso_FSTopo.3857/metadata.json -e 'this.url="http://data.fs.usda.gov/geodata/rastergateway/states-regions/states_zoom.php?stateID=ca"'
